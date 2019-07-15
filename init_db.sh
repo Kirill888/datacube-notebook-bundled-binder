805ed78c55795e3f0b06283b56e6ebfd3aba1b53
Kirill Kouzoubov <kirill888@gmail.com>
2019-07-15 16:30:59 +1000

    Bootstrap db from pg_dump

--- init_db.sh
+++ init_db.sh
@@ -2,14 +2,18 @@
 
 set -eux
 
+DB_DUMP_URL=${DB_DUMP_URL-"https://ga-aws-dea-dev-users.s3.amazonaws.com/kk/datacube-db.tar.xz"}
+
 # PostgreSQL Database initialisation
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DATABASE=$(awk '/db_database:/{print $2}' $THISDIR/.datacube.conf)
+DATABASE=$(awk '/db_database:/{print $2}' "$THISDIR/.datacube.conf")
 
 createdb -O $USER $DATABASE
-
 datacube system init
 
-#Further Database preparation, e.g. product addition and dataset indexing
-#psql -d $DATABASE -U $USERNAME -f $THISDIR/seed_db.sql
+if [ -n "${DB_DUMP_URL}" ]; then
+    dropdb "${DATABASE}"
+    createdb "${DATABASE}"
+    curl --silent "${DB_DUMP_URL}" | xz -d | pg_restore -d "${DATABASE}" || true
+fi
